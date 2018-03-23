b881f84b29051ac7a5044123cbb757121aeca1d6
Arek Ruta <arek.ruta89@gmail.com>
2018-03-23 16:18:02 +0100

    first chapter

---
+++ src/app/chapters/Chapter1.ts
@@ -0,0 +1,12 @@
+import {
+  Chapter,
+  CharacterAction,
+  FailureOptions,
+  SuccessOptions
+} from '../models/chapter';
+import { Weapon, Armor, Monster, Warrior } from '../models/characters';
+import {
+  GenderOptions,
+  RaceOptions,
+  ClassOptions
+} from '../models/character-options';
